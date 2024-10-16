3774730abac0981f1d26faf19ff5f1838a20e47e
ThatDudeCR01 <124479695+ThatDudeCR01@users.noreply.github.com>
2024-10-16 02:27:35 -0600

    add fecha_cobro to the model tareaMantenimiento

--- src/models/tarea-mantenimiento.js
+++ src/models/tarea-mantenimiento.js
@@ -1,16 +1,22 @@
 const mongoose = require("mongoose");
 
-const tareaMantenimientoSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  tipo: { type: String, required: true },
-  descripcion: { type: String, required: true },
-  duracion: { type: String, required: true },
-  template_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Template",
-    required: true,
-  },
-});
+const tareaMantenimientoSchema = new mongoose.Schema(
+  {
+    nombre: { type: String, required: true },
+    tipo: { type: String, required: true },
+    descripcion: { type: String, required: true },
+    duracion: { type: String, required: true },
+    fecha_cobro: { type: Date, required: true },
+    template_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Template",
+      required: true,
+    },
+  }
+  // {
+  //   timestamps: true,
+  // }
+);
 
 const TareaMantenimiento = mongoose.model(
   "TareaMantenimientos",
