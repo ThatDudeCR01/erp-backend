189addcc530edf353a1201d536e7f40dd683be0f
ThatDudeCR01 <124479695+ThatDudeCR01@users.noreply.github.com>
2024-10-15 21:06:00 -0600

    mantenimiento agregado estados, informacion adicional en tareas

--- src/models/mantenimiento.js
+++ src/models/mantenimiento.js
@@ -11,12 +11,15 @@ const mantenimientoSchema = new mongoose.Schema({
     ref: "Template",
     required: true,
   },
+  estado: { type: String, default: "pendiente" },
   tareas: [
     {
+      tareaMantenimiento_id: { type: String, required: true },
       nombre: { type: String, required: true },
       tipo: { type: String, required: true },
       descripcion: { type: String, required: true },
       duracion: { type: String, required: true },
+      template_id: { type: String, required: true },
     },
   ],
 });
